dd1cdcbe439aae398cd619be973eaced23801a30
manish <mahasethmanish63@gmail.com>
2026-05-12 22:18:48 +0545

    change print hello world

--- mysite/settings/settings.py
+++ mysite/settings/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "analytics_module",
     "sod_eod_module",
 ]
-print("hello womanisSSShrldd")
+print("hello womsdfanisSSShrldd")
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
