c1f51a2077ad2ded5eb500530cc627bcbd92d6ba
manish <mahasethmanish63@gmail.com>
2026-05-12 23:04:45 +0545

    git push

--- mysite/settings/settings.py
+++ mysite/settings/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "analytics_module",
     "sod_eod_module",
 ]
-print("hello womsdfanisdfssdfSSmanishSdsdfhrldd")
+print("hello womsdfanisdfsdfssdfSSmanishSdsdfhrldd")
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
