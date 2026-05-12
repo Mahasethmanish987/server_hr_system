abf8b2fd55f7aed943eedb022d6229872a5ca20d
manish <mahasethmanish63@gmail.com>
2026-05-12 21:39:53 +0545

    hello world

--- mysite/settings/settings.py
+++ mysite/settings/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "analytics_module",
     "sod_eod_module",
 ]
-
+print("hello world")
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
