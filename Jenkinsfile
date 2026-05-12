8a1790789649a84085f148228cba6882ff49a4c2
manish <mahasethmanish63@gmail.com>
2026-05-12 15:23:29 +0545

    replace config with os.environ.get

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,3 +1,8 @@
+def COLOR_MAP = [
+    'SUCCESS' : 'good',
+    'FAILURE' : 'danger',
+    ]
+
 pipeline {
     agent { label 'jenkinsrunner' }
 
@@ -29,25 +34,6 @@ pipeline {
             }
         }
 
-       stage('SonarQube Analysis') {
-    steps {
-        script {
-            def scannerHome = tool 'sonar8.0'
-
-            withSonarQubeEnv('sonarserver') {
-               sh """
-${scannerHome}/bin/sonar-scanner \
--Dsonar.projectKey=server_hr_system \
--Dsonar.sources=. \
--Dsonar.python.version=3.11 \
--Dsonar.sourceEncoding=UTF-8 \
--Dsonar.python.flake8.reportPaths=checkstyle-result.xml \
--Dsonar.exclusions=**/static/**,**/tests/**
-"""
-            }
-        }
-    }
-}
                      
 
         stage('Run Tests') {
@@ -58,19 +44,54 @@ ${scannerHome}/bin/sonar-scanner \
                         set -e
                         chmod +x ./entrypoint.sh
                         cp "$TEMP_ENV" .env
+                        docker compose build --no-cache
                         docker compose up -d postgres redis
                         sleep 5
-                        docker compose run --rm web pytest 
+                        docker compose run --rm web pytest --cov=. --cov-report=xml 
                     '''
                 }
             }
         }
+        stage('SonarQube Analysis') {
+    steps {
+        script {
+            def scannerHome = tool 'sonar8.0'
+
+            withSonarQubeEnv('sonarserver') {
+                sh """
+${scannerHome}/bin/sonar-scanner \
+-Dsonar.projectKey=server_hr_system \
+-Dsonar.sources=. \
+-Dsonar.python.version=3.11 \
+-Dsonar.sourceEncoding=UTF-8 \
+-Dsonar.python.flake8.reportPaths=checkstyle-result.xml \
+-Dsonar.python.coverage.reportPaths=coverage.xml \
+-Dsonar.exclusions=**/static/**,**/tests/**
+"""
+            }
+        }
+    }
+}
+stage("Quality Gate") {
+    steps {
+        script {
+            timeout(time: 1, unit: 'HOURS') {
+                waitForQualityGate abortPipeline: true
+            }
+        }
+    }
+}
     }
 
     post {
         always {
             sh 'docker compose down -v'
             sh 'rm -f .env'
+            echo "slack notification"
+            slackSend( 
+                channel: '#monitoring-alerts',
+                color: COLOR_MAP[currentBuild.currentResult],
+                message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}")
         }
         success {
             echo "All tests passed!"
--- mysite/settings/settings.py
+++ mysite/settings/settings.py
@@ -1,117 +1,107 @@
-
-
+import os
+from datetime import timedelta
 from pathlib import Path
-from datetime  import timedelta 
 
-import os 
-
-
-from decouple import config, Csv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY =config("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
+DEBUG = os.environ.get("DEBUG")
 
 
-DEBUG = config("DEBUG", cast=bool,default=False)
-
-
-ALLOWED_HOSTS = ['hr-system-99ym.onrender.com','127.0.0.1']
-
-
+ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'silk',
-    'employee_module',
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "silk",
+    "employee_module",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "django_filters",
-    'task',
-    'attendance_module',
-    'django_celery_results',
-    'django_celery_beat',
-    'leave_module',
-    'drf_spectacular_sidecar',
-    'drf_spectacular',
-    'payroll_module',
-    'analytics_module',
-    'sod_eod_module'
+    "task",
+    "attendance_module",
+    "django_celery_results",
+    "django_celery_beat",
+    "leave_module",
+    "drf_spectacular_sidecar",
+    "drf_spectacular",
+    "payroll_module",
+    "analytics_module",
+    "sod_eod_module",
 ]
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'silk.middleware.SilkyMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "silk.middleware.SilkyMiddleware",
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = "mysite.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
-
+WSGI_APPLICATION = "mysite.wsgi.application"
 
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config('DATABASE_NAME'),
-        "USER": config('DATABASE_USER'),
-        "PASSWORD": config('DATABASE_PASSWORD'),
-        "HOST": config('DATABASE_HOST'),
-        "PORT": '5432',
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": "5432",
     }
-  }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Kathmandu'
+TIME_ZONE = "Asia/Kathmandu"
 
 USE_I18N = True
 
@@ -119,36 +109,34 @@ USE_TZ = True
 
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://hr-system-99ym.onrender.com',
-    'http://localhost:3000',  
-    'http://127.0.0.1:3000',  
+    "https://hr-system-99ym.onrender.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",    
+    "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://hr-system-99ym.onrender.com", 
+    "https://hr-system-99ym.onrender.com",
 ]
-STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR/'static'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
-MEDIA_URL='media/'
-MEDIA_ROOT=BASE_DIR/'media'
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_FILTER_BACKENDS": (
-        "django_filters.rest_framework.DjangoFilterBackend",
-    ),
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema", 
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
@@ -159,9 +147,8 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",    
+    "http://localhost:3000",
     "http://127.0.0.1:3000",
-   
 ]
 CORS_ALLOW_CREDENTIALS = True
 
@@ -173,4 +160,4 @@ CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "Asia/Kathmandu"
 CELERY_ENABLE_UTC = True
 CELERY_TASK_RESULT_EXPIRES = 3600
-CELERY_RESULT_BACKEND = 'django-db'
+CELERY_RESULT_BACKEND = "django-db"
