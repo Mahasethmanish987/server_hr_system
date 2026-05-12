2688eb6e0aad7f6fe1d66282de84b83922f5c5fe
manish <mahasethmanish63@gmail.com>
2026-05-12 22:40:36 +0545

    check pipeline

--- mysite/settings/settings.py
+++ mysite/settings/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "analytics_module",
     "sod_eod_module",
 ]
-print("hello womsdfanisdfssdfSSSdsdfhrldd")
+print("hello womsdfanisdfssdfSSmanishSdsdfhrldd")
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
