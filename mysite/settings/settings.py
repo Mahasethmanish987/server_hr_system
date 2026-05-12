2394f2f5832feaf00a9720c198a4cd75ef9f3378
manish <mahasethmanish63@gmail.com>
2026-05-12 22:22:46 +0545

    change github plugin

--- mysite/settings/settings.py
+++ mysite/settings/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "analytics_module",
     "sod_eod_module",
 ]
-print("hello womsdfanisSSShrldd")
+print("hello womsdfanisdfsSSShrldd")
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
